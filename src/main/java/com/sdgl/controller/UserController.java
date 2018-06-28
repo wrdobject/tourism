37dffe747ca460a2cf4f5d9bd7cf2537dee55250
wrd <wrd_object@163.com>
2018-06-28 18:00:43 +0800

    添加controller里的方法

--- src/main/java/com/sdgl/controller/UserController.java
+++ src/main/java/com/sdgl/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.sdgl.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+
+    @RequestMapping("/")
+    public String show(){
+
+        return "lay";
+    }
 }
