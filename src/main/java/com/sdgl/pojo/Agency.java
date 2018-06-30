e6c394fa15e6b3dad1b15ebaf7c09088f7fcc65d
Git6226 <15035726226.163.c0m>
2018-06-30 09:55:15 +0800

    供应商实体类 乔紫阳

---
+++ src/main/java/com/sdgl/pojo/Agency.java
@@ -0,0 +1,21 @@
+package com.sdgl.pojo;
+
+import java.util.Date;
+
+public class Agency {
+    private Integer agency_id;
+    private String agency_name;
+    private String agency_abbreviation;
+    private String agency_type;
+    private String agency_email;
+    private String agency_fax;
+    private String agency_phone;
+    private String agency_province;
+    private String agency_city;
+    private String agency_county;
+    private String agency_address;
+    private String agency_synopsis;
+    private Date agency_time;
+    private Integer agency_owner;
+
+}
