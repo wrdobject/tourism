db277c24c930f036bfbff5b71a36fc9e322b8a17
Git6226 <15035726226.163.c0m>
2018-06-30 10:00:15 +0800

    供应商实体类 乔紫阳

---
+++ src/main/java/com/sdgl/pojo/BusFleet.java
@@ -0,0 +1,21 @@
+package com.sdgl.pojo;
+
+
+import java.util.Date;
+
+public class BusFleet {
+    private Integer bus_fleet_id;
+    private String bus_fleet_name;
+    private String bus_fleet_abbreviation;
+    private String bus_fleet_email;
+    private String bus_fleet_fax;
+    private String bus_fleet_phone;
+    private String bus_fleet_province;
+    private String bus_fleet_city;
+    private String bus_fleet_county;
+    private String bus_fleet_address;
+    private String bus_flee_tintroduce;
+    private Date bus_fleet_time;
+    private Integer bus_fleet_owner;
+
+}
---
+++ src/main/java/com/sdgl/pojo/Vehicle.java
@@ -0,0 +1,15 @@
+package com.sdgl.pojo;
+
+
+import java.util.Date;
+
+public class Vehicle {
+    private Integer vehicle_id;
+    private Integer bus_fleet_id;
+    private String vehicle_number;
+    private String vehicle_type;
+    private String vehicle_img;
+    private Date vehicle_time;
+    private Integer vehicle_owner;
+
+}
