83e436877a981a7c47c1907bdb94429c2b80f88d
szx8199 <18434399862@163.com>
2018-07-09 09:37:30 +0800

    1组尹誉超更新页面效果

---
+++ src/main/resources/static/layui/js/one/global.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function NewPage(page){
+	window.open(page,"frmright");
+}
+
+function openPage(page){
+	window.open(page);
+}
---
+++ src/main/resources/static/layui/js/one/indexs.js
@@ -0,0 +1,28 @@
+$(function(){
+	var LODOP;
+});
+function printview(){
+	createPage();
+	LODOP.PREVIEW(); //打印预览
+}
+function updatePrint(){
+	createPage();
+	LODOP.PRINT_SETUP();//打印维护
+}
+function print(){
+	createPage();
+	LODOP.PRINT_DESIGN();//直接打印
+}
+
+function createPage(){
+	
+	LODOP=getLodop();  //获取打印对象
+	var style="<style>"+document.getElementById("style").innerHTML+"</style>";
+	var html=style+"<body>"+document.getElementById("dy").innerHTML+"</body>";
+	LODOP.SET_PRINT_STYLE("Bold",1); //字体加粗
+	LODOP.ADD_PRINT_HTM(50,30,200,890,html); //打印HTML
+
+}
+
+
+	//LODOP.ADD_PRINT_TEXT(50,50,50,50,"已打印"); //打印文字
